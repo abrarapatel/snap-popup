8b40349ab9bf8b5eeb86fb796cccd44cccbb3ef4
abrarapatel <abrar44120@gmail.com>
2024-08-18 16:39:50 -0400

    updated

--- snap-popup.js
+++ snap-popup.js
@@ -172,6 +172,7 @@ const snap = {
             htmlCode = "",
             width = 450,
         } = options;
+        
 
         let iconSVGCode = "";
 
@@ -191,10 +192,6 @@ const snap = {
 
         if (closeButton) {
             snapPopupBaseContentHTML = `${snapPopupBaseContentHTML}<div class="snap-popup-close" onclick="snap.close()"><div>&times;</div></div>`;
-        }
-
-        if (type == "success") {
-            snapPopupBaseContentHTML = `<center><dotlottie-player src="https://lottie.host/0aac02c7-65b1-4a58-94ff-15c2a224a7fa/WwIBZNcHOQ.json" background="transparent" speed="3" style="width: 150px; height: 150px;" autoplay></dotlottie-player></center>`;
         }
 
         if (header != "") {
